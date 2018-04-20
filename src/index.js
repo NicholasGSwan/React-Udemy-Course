b67cc5da9eef976fd93eb87d180d5beb1a08c317
Nicholas Swan <nicholasgswan@gmail.com>
2018-04-19 21:42:51 -0500

    finished first video app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
@@ -20,19 +21,26 @@ class App extends Component{
       selectedVideo: null
     };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({
-        videos: videos,
-        selectedVideo: videos[0]
-      });
-      // this.setState({ videos: videos }); < == above resolves to this
-    });
+  this.videoSearch('surfboards');
   }
 
+  videoSearch(term){
+      YTSearch({key: API_KEY, term: term}, (videos) => {
+        this.setState({
+          videos: videos,
+          selectedVideo: videos[0]
+        });
+      });
+    }
+
+
+
   render(){
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
         //call back function passed to VideoList then VideoListItem
